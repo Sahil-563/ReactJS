6f8f3eba2fb5245f237640261fd84d98a9eef669
Sahil <sahil2002ece@gmail.com>
2023-08-24 14:11:26 +0530

    My first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+import Classcomponent from './Classcomponent';
 function App() {
+  function InsideFunctionalComponent(){
+    return(
+      <h3>Insider Functional Component</h3>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar />
+        <Detail />
+        <Classcomponent />
+        {/* {InsideFunctionalComponent()} */}
+        <InsideFunctionalComponent />
     </div>
+   
   );
 }
 
+//We can use differet functions in same file
+function Detail(){
+  return(
+      <> 
+      You can use different functions in same file
+      <h2>We can use another function inside a functioinal component but we cannot use that component outside</h2>
+      </>
+      
+  )
+}
+
+// We can create anohter function in same file
 export default App;
---
+++ src/Classcomponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Classcomponent extends React.Component{
+    render(){
+        return(
+            <h1>Hello My dear friends</h1>
+        )
+    }
+}
+export default Classcomponent
